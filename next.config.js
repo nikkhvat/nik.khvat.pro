7b7a71db036095945322cefa1441cf2ce4f13476
Nikita Khvatov <nikhvatov19@gmail.com>
2023-04-19 13:41:42 +0600

    add preact

--- next.config.js
+++ next.config.js
@@ -24,6 +24,18 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
+
 }
 
 module.exports = nextConfig
