d205ce49843d9246a2c6ddc3ab4543b229955170
Nikita Khvatov <nikhvatov19@gmail.com>
2023-05-01 18:39:21 +0600

    fix storage for ssr

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,13 +1,17 @@
 class Storage {
     static get(key: string) {
+        if (typeof window === 'undefined') return "not use localStorage on server"
+        
         return localStorage.getItem(`nik19ta_${key}`);
     };
 
     static set(key: string, payload: any) {
+        if (typeof window === 'undefined') return "not use localStorage on server"
         return localStorage.setItem(`nik19ta_${key}`, payload);
     };
 
     static delete(key: string) {
+        if (typeof window === 'undefined') return "not use localStorage on server"
         return localStorage.removeItem(`nik19ta_${key}`);
     };
 }
