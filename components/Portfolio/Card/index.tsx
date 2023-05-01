8504160fc3a30ef7c5ec1af2029f49f665c3b3dc
Nikita Khvatov <nikhvatov19@gmail.com>
2023-05-01 19:17:42 +0600

    add avif and webp for preview

--- components/Portfolio/Card/index.tsx
+++ components/Portfolio/Card/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 import styles from "./index.module.css";
 
@@ -16,19 +15,15 @@ interface Props {
 }
 
 const PortfolioCard: React.FC<Props> = ({ card }) => {
-  const loaderProp = ({ src }: any) => src;
-
   return (
     <div key={card.id} className={styles.portfolio_cards__item}>
       <Link href={`/project/${card.id}`} target="_blank">
-        <Image
-          width={635.5}
-          height={328}
-          loader={loaderProp}
-          src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}/${card.url}`}
-          className={styles.portfolio_cards__item_photo}
-          alt={`preiew for ${card.title}`}
-        />
+
+        <picture >
+          <source className={styles.portfolio_cards__item_photo} type="image/avif" srcSet={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}/${card.url}.avif`} />
+          <source className={styles.portfolio_cards__item_photo} type="image/webp" srcSet={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}/${card.url}.webp`} />
+          <img className={styles.portfolio_cards__item_photo}  src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}/${card.url}.jpg`} alt={`preiew for ${card.title}`} />
+        </picture>
 
         <p className={styles.portfolio_cards__item_title}>{card.title}</p>
         <p className={styles.portfolio_cards__item_subtitle}>
