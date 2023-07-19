dd9d57e5800e7cdf6538fe68f28520be6ef0b967
Nikita Khvatov <nikhvatov19@gmail.com>
2023-07-19 16:26:47 +0600

    fix stats service

--- components/Stat/index.tsx
+++ components/Stat/index.tsx
@@ -4,28 +4,38 @@ const Stat = () => {
   useEffect(() => {
     const extendTimeSpent = async (session: string) => {
       localStorage.stat_visit_session = session
-      const url = `${process.env.NEXT_PUBLIC_BACK_END}/api/stat/update/visit/extend?session=${session}`
+      const url = `https://gostat.app/api/stats/set/visit/extend?session=${session}&app_id=b871fdf8-3de0-4b9c-870a-2e534cd0817c`;
       await fetch(url, { method: 'PUT', headers: { 'Content-Type': 'application/json' } })
 
       setTimeout(() => extendTimeSpent(session), 3000)
     }
-
-    const token = localStorage.stat_token
-
-    if (token) return
 
     const requestOptions: any = { method: 'PUT', redirect: 'follow' };
 
     // Delay 800 
     setTimeout(() => {
       if (localStorage.stat_visit_session) {
-        fetch(`${process.env.NEXT_PUBLIC_BACK_END}/api/stat/update/visit?un=1&url=${window.location.pathname}&title=${document.querySelector("title")?.innerHTML}&session=${localStorage.stat_visit_session}`, requestOptions)
-          .then(data => data.json())
-          .then(data => extendTimeSpent(data.session))
+        fetch(
+          `https://gostat.app/api/stats/set/visit?un=1&url=${
+            window.location.pathname
+          }&title=${document.querySelector("title")?.innerHTML}&session=${
+            localStorage.stat_visit_session
+          }&app_id=b871fdf8-3de0-4b9c-870a-2e534cd0817c`,
+          requestOptions
+        )
+          .then((data) => data.json())
+          .then((data) => extendTimeSpent(data.session));
       } else {
-        fetch(`${process.env.NEXT_PUBLIC_BACK_END}/api/stat/update/visit?un=0&url=${window.location.pathname}&title=${document.querySelector("title")?.innerHTML}`, requestOptions)
-          .then(data => data.json())
-          .then(data => extendTimeSpent(data.session))
+        fetch(
+          `https://gostat.app/api/stats/set/visit/extend?un=0&url=${
+            window.location.pathname
+          }&title=${
+            document.querySelector("title")?.innerHTML
+          }&app_id=b871fdf8-3de0-4b9c-870a-2e534cd0817c`,
+          requestOptions
+        )
+          .then((data) => data.json())
+          .then((data) => extendTimeSpent(data.session));
       }
     }, 800)
 
